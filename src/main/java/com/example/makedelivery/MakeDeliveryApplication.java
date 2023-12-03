be17a055d5d2070d9f76e7793f6ecd2bd6e6f1ad
JeonDaehong <daehong770@gmail.com>
2023-12-03 18:43:21 +0900

    Initial commit

---
+++ src/main/java/com/example/makedelivery/MakeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.makedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/makedelivery/MakeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
