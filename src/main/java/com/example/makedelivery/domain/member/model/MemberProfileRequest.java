31fadb4d28d72e43d25b9b51e847fb41818b43be
JeonDaehong <daehong770@gmail.com>
2023-12-10 22:57:48 +0900

    fix : [#6] NickName Size 수정

--- src/main/java/com/example/makedelivery/domain/member/model/MemberProfileRequest.java
+++ src/main/java/com/example/makedelivery/domain/member/model/MemberProfileRequest.java
@@ -1,6 +1,7 @@
 package com.example.makedelivery.domain.member.model;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 public class MemberProfileRequest {
 
     @NotEmpty(message = "닉네임은 공란일 수 없습니다.")
+    @Size(min = 2, max = 30, message = "닉네임은 최소 2자 이상, 최대 30자 이하여야 합니다.")
     private final String nickname;
 
 }
