84ca493ea8fd87e7dce57ea7531ccbddb468d482
JeonDaehong <daehong770@gmail.com>
2023-12-10 22:59:13 +0900

    docs : [#6] 주석 추가

--- src/main/java/com/example/makedelivery/domain/member/service/SessionLoginService.java
+++ src/main/java/com/example/makedelivery/domain/member/service/SessionLoginService.java
@@ -5,6 +5,9 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+  *  레디스의 키값으로 저장되는 타입입니다.
+ */
 @Service
 @RequiredArgsConstructor
 public class SessionLoginService implements LoginService {
@@ -25,13 +28,15 @@ public class SessionLoginService implements LoginService {
 
     @Override
     public Member getCurrentMember() {
-        Long memberId = (Long) session.getAttribute(MEMBER_ID);
+        // Redis 에서 Key 가 String 타입으로 직렬화, 역직렬화를 구현하였기 때문에
+        // 먼저 String 으로 만들어 준 후 Long 으로 형변환을 해줘야 합니다.
+        Long memberId = Long.parseLong(String.valueOf(session.getAttribute(MEMBER_ID)));
         return memberService.findMemberById(memberId);
     }
 
     @Override
     public Long getCurrentMemberId() {
-        return (Long) session.getAttribute(MEMBER_ID);
+        return Long.parseLong(String.valueOf(session.getAttribute(MEMBER_ID)));
     }
 
 
