4030a9219733b5cce93f4720fd6c30ceb0a56214
JeonDaehong <daehong770@gmail.com>
2023-12-10 22:55:52 +0900

    feat : [#6] URI 추가

--- src/main/java/com/example/makedelivery/common/util/URIConstants.java
+++ src/main/java/com/example/makedelivery/common/util/URIConstants.java
@@ -3,4 +3,5 @@ package com.example.makedelivery.common.util;
 public class URIConstants {
 
     public static final String MEMBER_API_URI = "/api/member";
+    public static final String STORE_API_URI = "/api/store";
 }
