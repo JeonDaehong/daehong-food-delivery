649590abc8e741273e216cd856b5a77836c2fd98
JeonDaehong <daehong770@gmail.com>
2023-12-13 15:02:30 +0900

    feat : [#7] Delete 상태인 주소 자동 삭제 스케쥴러 추가

---
+++ src/main/java/com/example/makedelivery/common/scheduler/AddressScheduler.java
@@ -0,0 +1,26 @@
+package com.example.makedelivery.common.scheduler;
+
+import com.example.makedelivery.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AddressScheduler {
+
+    private final MemberService memberService;
+
+    /**
+     * Status 가 "삭제" 상태인 주소들을 DB 에서 실제로 삭제합니다.
+     * <br><br>
+     * 회원정보나, 매장정보등은 스케쥴러가 아니라 운영자들이 직접 확인하고 삭제하게끔 설계하였습니다.
+     * 반면 주소정보는 회원, 매장 정보에 비해 중요도가 낮고, 빠르게 데이터가 삭제되어도
+     * 문제가 되지 않을 요소이기 때문에, 스케쥴러를 통하여 관리하도록 설계하였습니다.
+     */
+    @Scheduled(fixedDelay = 1000 * 60 * 10) // 10분에 한 번 실행
+    public void deleteAllAddressDeleteStatus() {
+        memberService.deleteAllAddressDeleteStatus();
+    }
+
+}
