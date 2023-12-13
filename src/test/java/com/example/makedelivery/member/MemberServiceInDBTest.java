8e75f7cb23c17cc4fce02d675f2aed83c2f4e398
JeonDaehong <daehong770@gmail.com>
2023-12-13 15:04:19 +0900

    test : [#7] 회원 프로필 업데이트, 회원 삭제 테스트 케이스 추가

--- src/test/java/com/example/makedelivery/member/MemberServiceInDBTest.java
+++ src/test/java/com/example/makedelivery/member/MemberServiceInDBTest.java
@@ -1,9 +1,10 @@
 package com.example.makedelivery.member;
 
 import com.example.makedelivery.common.annotation.LoginCheck.MemberLevel;
+import com.example.makedelivery.common.exception.MemberNotFoundException;
 import com.example.makedelivery.domain.member.model.MemberJoinRequest;
+import com.example.makedelivery.domain.member.model.MemberProfileRequest;
 import com.example.makedelivery.domain.member.model.entity.Member;
-import com.example.makedelivery.domain.member.repository.MemberRepository;
 import com.example.makedelivery.domain.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
- * 일반적으로는 Test DB 와 연결하여 테스트를 진행합니다.
+ * Test DB 와 연결하여 테스트를 진행합니다.
  */
 @SpringBootTest
-public class MemberServiceJoinTest {
+public class MemberServiceInDBTest {
 
     @Autowired
     private MemberService memberService;
@@ -29,7 +31,7 @@ public class MemberServiceJoinTest {
     @Test
     @DisplayName("회원가입이 정상적으로 진행됩니다.")
     @Transactional
-    public void testJoin() {
+    void testJoin() {
         // given
         MemberJoinRequest joinRequest = new MemberJoinRequest("aTestAdmin710a@admin.co.kr",
                 "1a2a3a4a5a!@#","TestAdmin", MemberLevel.MEMBER);
@@ -39,6 +41,30 @@ public class MemberServiceJoinTest {
         Member dbMember = memberService.findMemberByEmail("aTestAdmin710a@admin.co.kr");
         // then
         assertThat(dbMember.getEmail()).isEqualTo(member.getEmail());
+    }
+
+    @Test
+    @DisplayName("프로필 업데이트가 정상적으로 진행됩니다.")
+    @Transactional
+    void testUpdateProfile() {
+        // given
+        MemberProfileRequest profileRequest = new MemberProfileRequest("TestAdminUpdate");
+        Member dbMember = memberService.findMemberByEmail("aTestAdmin710a@admin.co.kr");
+        // when
+        memberService.updateMemberProfile(dbMember, profileRequest);
+        // then
+        assertThat(dbMember.getNickname()).isEqualTo(profileRequest.getNickname());
+    }
+
+    @Test
+    @DisplayName("회원 정보가 정상적으로 삭제되고, 다시 재조회 시 MemberNotFoundException 이 출력되야 합니다.")
+    @Transactional
+    void testDelete() {
+        Member dbMember = memberService.findMemberByEmail("aTestAdmin710a@admin.co.kr");
+        memberService.deleteMember(dbMember);
+        assertThrows(MemberNotFoundException.class, () -> {
+            Member member = memberService.findMemberByEmail("aTestAdmin710a@admin.co.kr");
+        });
 
     }
 
