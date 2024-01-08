f0b85d06fcdcf9f6e726cb5c2b377f38a510d648
JeonDaehong <daehong770@gmail.com>
2024-01-08 18:02:52 +0900

    docs : [#11] @Transactional(readOnly = true) 관련 주석 추가

--- src/main/java/com/example/makedelivery/domain/member/service/MemberService.java
+++ src/main/java/com/example/makedelivery/domain/member/service/MemberService.java
@@ -35,6 +35,12 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+    /**
+     * 읽기 전용 서비스에는 @Transactional(readOnly = true) 를 추가해주었습니다.
+     * 해당 어노테이션을 붙이는 이유는 오직 읽기 작업(SELECT)만 수행한다는 걸 명시적으로 알리기 위함도 있고,
+     * JPA 의 더티체킹 변경감지가 일어나지 않아 메모리를 절약할 수 있으며,
+     * 트랜잭션 ID를 부여하지 않으므로 오버헤드가 일어나지 않아 성능상 이점이 생기기 때문입니다.
+     */
     @Transactional(readOnly = true)
     public void existsByEmail(String email) {
         if ( memberRepository.existsByEmail(email) ) throw new ApiException(DUPLICATED_EMAIL);
