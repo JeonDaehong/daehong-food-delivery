3aea8dd1fca9cbc1a3d27256bd8ba80818c0289e
JeonDaehong <daehong770@gmail.com>
2024-01-23 13:19:30 +0900

    chore : [#14] 결제 서비스에 Transactional 추가

--- src/main/java/com/example/makedelivery/domain/order/service/TossPaymentService.java
+++ src/main/java/com/example/makedelivery/domain/order/service/TossPaymentService.java
@@ -1,5 +1,6 @@
 package com.example.makedelivery.domain.order.service;
 
+import com.example.makedelivery.common.exception.ApiException;
 import com.example.makedelivery.domain.order.domain.entity.Payment;
 import com.example.makedelivery.domain.order.domain.entity.Payment.PaymentStatus;
 import com.example.makedelivery.domain.order.domain.entity.Payment.PaymentType;
@@ -7,6 +8,9 @@ import com.example.makedelivery.domain.order.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.makedelivery.common.exception.ExceptionEnum.PAYMENT_INFO_NOT_FOUND;
 
 @Service
 @Slf4j
@@ -16,6 +20,7 @@ public class TossPaymentService implements PaymentService {
     private final PaymentRepository paymentRepository;
 
     @Override
+    @Transactional
     public void payment(long orderId, long memberId, int price) {
         Payment payment = Payment.builder()
                 .price(price)
@@ -26,4 +31,5 @@ public class TossPaymentService implements PaymentService {
                 .build();
         paymentRepository.save(payment);
     }
+
 }
