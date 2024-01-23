0d7056ff29b105021639990047084e2971be5714
JeonDaehong <daehong770@gmail.com>
2024-01-23 11:04:12 +0900

    feat : [#13] 새로운 유형의 쿠폰 추가

---
+++ src/main/java/com/example/makedelivery/domain/coupon/service/FebruaryEventCouponUseService.java
@@ -0,0 +1,18 @@
+package com.example.makedelivery.domain.coupon.service;
+
+import com.example.makedelivery.domain.coupon.domain.entity.Coupon;
+import com.example.makedelivery.domain.member.model.entity.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class FebruaryEventCouponUseService implements CouponUseService {
+    @Override
+    public void useCoupon(Member member, Coupon coupon) {
+        coupon.useCoupon();
+        member.addPoint(3_000);
+    }
+}
